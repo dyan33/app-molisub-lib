1ac557857eea8a1ac443d2170301f5ef72f9dbdc
liuhao <liuhaoliter@gmail.com>
2019-09-11 15:03:27 +0800

    提交

--- app/src/main/java/com/enhtmv/molisub/MainActivity.java
+++ app/src/main/java/com/enhtmv/molisub/MainActivity.java
@@ -1,16 +1,9 @@
 package com.enhtmv.molisub;
 
-import android.content.DialogInterface;
-import android.content.Intent;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
 import com.cp.plugin.Plugin;
 import com.cp.plugin.event.SubEvent;
-import com.enhtmv.sublib.common.sub.Sub;
-
-import static android.provider.Settings.ACTION_NOTIFICATION_LISTENER_SETTINGS;
 
 public class MainActivity extends AppCompatActivity implements SubEvent {
 
@@ -20,17 +13,6 @@ public class MainActivity extends AppCompatActivity implements SubEvent {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        if (BuildConfig.DEBUG) {
-//            SubProxy proxy = HostUtil.proxy();
-//            this.setProxy(new SubProxy("37.48.98.160", "engineer@foxseek.com", "0c4263", 11285));
-            Plugin.proxy("91.220.77.154", "mauritius", "precpx123", 8090);
-//            Plugin.proxy(proxy.getHost(), proxy.getUsername(), proxy.getPassword(), proxy.getPort());
-//            Plugin.proxy("37.48.98.160", "engineer@foxseek.com", "0c4263", 11303);
-//            Plugin.log(true);
-//            Plugin.closeWifi(false);
-//            Plugin.setHiden(false);
-            Plugin.operator("52000");
-        }
 
         Plugin.init(this, this);
 
@@ -42,27 +24,6 @@ public class MainActivity extends AppCompatActivity implements SubEvent {
     protected void onResume() {
         super.onResume();
         Plugin.call();
-    }
-
-    public AlertDialog buildNotificationAlert(String title, String content, String yes, String no) {
-        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
-        alertDialogBuilder.setCancelable(false);
-        alertDialogBuilder.setTitle(title);
-        alertDialogBuilder.setMessage(content);
-        alertDialogBuilder.setPositiveButton(yes,
-                new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int id) {
-                        startActivity(new Intent(ACTION_NOTIFICATION_LISTENER_SETTINGS));
-                        //go to set permision
-                    }
-                });
-        alertDialogBuilder.setNegativeButton(no,
-                new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int id) {
-                        //cancel
-                    }
-                });
-        return (alertDialogBuilder.create());
     }
 
     @Override
@@ -77,4 +38,3 @@ public class MainActivity extends AppCompatActivity implements SubEvent {
 
 }
 
-//adb shell settings put global http_proxy 192.168.31.112:8090
