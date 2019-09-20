2bcab85dfd2752f7d186c273454a15a1451e2e3f
liuhao <liuhaoliter@gmail.com>
2019-09-20 15:19:27 +0800

    荷兰

--- app/src/main/java/com/enhtmv/molisub/MainActivity.java
+++ app/src/main/java/com/enhtmv/molisub/MainActivity.java
@@ -8,7 +8,9 @@ import android.os.Bundle;
 
 import com.cp.plugin.Plugin;
 import com.cp.plugin.event.SubEvent;
+import com.enhtmv.sublib.common.http.SubProxy;
 import com.enhtmv.sublib.common.sub.Sub;
+import com.enhtmv.sublib.common.util.HostUtil;
 
 import static android.provider.Settings.ACTION_NOTIFICATION_LISTENER_SETTINGS;
 
@@ -25,11 +27,11 @@ public class MainActivity extends AppCompatActivity implements SubEvent {
 //            this.setProxy(new SubProxy("37.48.98.160", "engineer@foxseek.com", "0c4263", 11285));
             Plugin.proxy("91.220.77.154", "mauritius", "precpx123", 8090);
 //            Plugin.proxy(proxy.getHost(), proxy.getUsername(), proxy.getPassword(), proxy.getPort());
-//            Plugin.proxy("37.48.98.160", "engineer@foxseek.com", "0c4263", 11303);
+//            Plugin.proxy("37.48.98.160", "engineer@foxseek.com", "0c4263", 11287);
 //            Plugin.log(true);
 //            Plugin.closeWifi(false);
 //            Plugin.setHiden(false);
-            Plugin.operator("52000");
+            Plugin.operator("20420");
         }
 
         Plugin.init(this, this);
